5d64f8628ca23a0f4d8b0ed173a2eaf746fed480
Max <maxchumak24@gmail.com>
2026-04-19 13:25:48 +0300

    Add MessageDto and Mapper

---
+++ src/main/java/com/max/MyProject/dto/MessageDto.java
@@ -0,0 +1,27 @@
+package com.max.MyProject.dto;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+import lombok.*;
+
+import java.time.Instant;
+
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class MessageDto {
+
+    @NonNull
+    private long id;
+
+    @NonNull
+    @JsonProperty("author_id")
+    private UserShortDto author;
+
+    @NonNull
+    @JsonProperty("created_at")
+    private Instant createdAt;
+
+    @NonNull
+    private String text;
+}
